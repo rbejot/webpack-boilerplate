1c617f07a71b4d559b410a6962b6d59fe13f883c
Rodolphe <rbejot@student.42.fr>
2018-05-23 23:06:46 +0200

    meta correction

--- metatag.js
+++ metatag.js
@@ -1,6 +1,6 @@
 export const url = "http://my-website.com";
 export const language = "en";
-export const title = "Marche";
+export const title = "Titile";
 export const description = "Description";
 export const image = "main-image.png";
 export const large_image = "large-image.png";
